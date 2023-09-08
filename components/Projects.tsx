f50a24a89bdcad853ba4f6ac040a28280edb234d
Kyle Thornton <20633426+KyleMThornton@users.noreply.github.com>
2023-09-08 16:07:09 -0700

    Added projects carousel

---
+++ components/Projects.tsx
@@ -0,0 +1,26 @@
+export default function Projects() {
+    return (
+        <div className="flex m-20">
+			<div className="carousel carousel-center rounded-box w-full">
+				<div className="carousel-item p-60 bg-slate-200">
+					<p>Fart</p>
+				</div>
+				<div className="carousel-item p-60 bg-slate-400">
+					<p>Fart</p>
+				</div> 
+				<div className="carousel-item p-60 bg-slate-600">
+					<p>Fart</p>
+				</div> 
+				<div className="carousel-item p-60 bg-slate-800">
+					<p>Fart</p>
+				</div>
+				<div className="carousel-item p-60 bg-slate-100">
+					<p>Fart</p>
+				</div>
+				<div className="carousel-item p-60 bg-slate-400">
+					<p>Fart</p>
+				</div>
+			</div>
+		</div>
+    )
+}
