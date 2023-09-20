692738973af6dc34f61400f2901b3e987d7fcfef
Kyle Thornton <20633426+KyleMThornton@users.noreply.github.com>
2023-09-20 16:24:32 -0700

    messin around, checkpoint before framer motion

--- components/HeaderText.tsx
+++ components/HeaderText.tsx
@@ -2,13 +2,13 @@ import { useEffect, useState } from "react"
 import TextTransition, { presets } from 'react-text-transition';
 
 export default function HeaderText() {
-    const wordsToDescribeKyle = ["Software Engineer", "Web Developer", "Handsome Devil", "Friend", "Foe?"]
+    const wordsToDescribeKyle = ["Hello!", "¡Hola!", "Bonjour!", "Guten Tag!"]
     const [index, setIndex] = useState(0);
 
     useEffect(() => {
         const intervalId = setInterval(
           () => setIndex((index) => index + 1),
-          4000, // every 4 seconds
+          2000, // every 4 seconds
         );
         return () => clearTimeout(intervalId);
     }, []);
@@ -16,11 +16,10 @@ export default function HeaderText() {
 
     return (
         <header className="flex justify-center h-screen items-center">
-            <div className="flex flex-col items-center">
-                <h1 className="text-7xl font-bold text-white">Kyle Thornton</h1>
-                <h2 className="text-4xl font-light pt-5">
-                    <TextTransition springConfig={presets.wobbly}>{wordsToDescribeKyle[index % wordsToDescribeKyle.length]}</TextTransition>
-                </h2>
+            <div className="flex flex-col p-5">
+                <h1 className="text-3xl text-black pb-1"><TextTransition springConfig={presets.wobbly}>{wordsToDescribeKyle[index % wordsToDescribeKyle.length]}</TextTransition></h1>
+                <h1 className="text-3xl text-black pb-1">I'm <span>Kyle Thornton</span>.</h1>
+                <h1 className="text-4xl font-bold text-black">A full stack web developer with a <span className="text-transparent bg-clip-text bg-gradient-to-br from-pink-500 to-red-800">passion for fashion.</span></h1>
             </div>
         </header>
     )
