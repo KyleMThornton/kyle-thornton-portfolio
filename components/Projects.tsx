28787b32086ff65c59bae0a1d043d1eefe8b34ff
Kyle Thornton <20633426+KyleMThornton@users.noreply.github.com>
2023-09-28 16:58:58 -0700

    Add container for spacing

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -2,7 +2,7 @@ import ProjectCard from "./ProjectCard";
 
 export default function Projects() {
     return (
-        <div className="flex flex-col h-auto bg-white px-36">
+        <div className="flex flex-col container p-4 h-auto">
 			<h2 className="text-4xl font-bold">Projects</h2>
 			<h3>A few of my recent projects.</h3>
 			<div className="flex flex-row flex-wrap justify-between">
