79d60f249e7f4867506971e608e31f34a47fdb99
Kyle Thornton <20633426+KyleMThornton@users.noreply.github.com>
2023-09-28 11:18:59 -0700

    Remove gradient background

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className='min-h-screen bg-gradient-to-b from-slate-50 to-gray-300'>
+    <html lang="en" className=''>
       <body className={inter.className}>{children}</body>
     </html>
   )
