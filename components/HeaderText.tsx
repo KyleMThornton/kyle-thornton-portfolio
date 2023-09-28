3168f90dd2ad190a70697bc38906af2bb8261db7
Kyle Thornton <20633426+KyleMThornton@users.noreply.github.com>
2023-09-28 11:18:38 -0700

    Remove screen sizing for header

--- components/HeaderText.tsx
+++ components/HeaderText.tsx
@@ -15,7 +15,7 @@ export default function HeaderText() {
 
 
     return (
-        <header className="flex justify-start h-screen items-center md:pl-36 pb-36">
+        <header className="flex justify-start md:pl-36 pb-36">
             <div className="flex flex-col p-5 md:w-4/5">
                 <h1 className="text-3xl md:text-4xl text-black pb-1 flex flex-row"><TextTransition springConfig={presets.wobbly}>{wordsToDescribeKyle[index % wordsToDescribeKyle.length]}</TextTransition> 👋🏻</h1>
                 <h1 className="text-3xl md:text-4xl text-black pb-2">I'm <span>Kyle Thornton</span>.</h1>
