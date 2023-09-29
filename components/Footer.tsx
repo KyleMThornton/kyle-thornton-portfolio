fad91afb6a2ba6377a6858bef4fbdaac1a74be46
Kyle Thornton <20633426+KyleMThornton@users.noreply.github.com>
2023-09-29 14:30:25 -0700

    Update footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="footer footer-center p-4 bg-gray-400 text-base-content">
+    <footer className="footer footer-center p-4 bg-base-300 text-base-content">
       <aside>
         <p>
           Made with ❤️ by{" "}
