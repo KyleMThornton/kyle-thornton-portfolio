70c6d421c6d3f46c5d4167aad78b590527f13e00
Kyle Thornton <20633426+KyleMThornton@users.noreply.github.com>
2023-09-29 14:25:32 -0700

    Add tooltips to tech section

--- components/Tech.tsx
+++ components/Tech.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { TbBrandNextjs } from "react-icons/tb";
 import { FaReact } from "react-icons/fa";
 import {
@@ -8,20 +10,46 @@ import {
   SiPrisma,
 } from "react-icons/si";
 import { BiLogoPostgresql } from "react-icons/bi";
+import { Tooltip } from 'react-tooltip';
+
 
 export default function Tech() {
   return (
     <div className="flex flex-col md:flex-row container justify-items-center p-4">
       <h2 className="text-2xl md:text-3xl">My favorite tech:</h2>
       <div className="flex flex-row items-center">
-        <TbBrandNextjs className="text-3xl mx-1" />
-        <FaReact className="text-3xl mx-1" />
-        <SiTailwindcss className="text-3xl mx-1" />
-        <BiLogoPostgresql className="text-3xl mx-1" />
-        <SiTypescript className="text-3xl mx-1" />
-        <SiNodedotjs className="text-3xl mx-1" />
-        <SiSupabase className="text-3xl mx-1" />
-        <SiPrisma className="text-3xl mx-1" />
+        <a className="nextjs"><TbBrandNextjs className="text-3xl mx-1" /></a>
+        <Tooltip anchorSelect=".nextjs" place="bottom">
+            NextJS
+        </Tooltip>
+        <a className="react"><FaReact className="text-3xl mx-1" /></a>
+        <Tooltip anchorSelect=".react" place="bottom">
+            React
+        </Tooltip>
+        <a className="tailwind"><SiTailwindcss className="text-3xl mx-1" /></a>
+        <Tooltip anchorSelect=".tailwind" place="bottom">
+            Tailwind CSS
+        </Tooltip>
+        <a className="postgres"><BiLogoPostgresql className="text-3xl mx-1" /></a>
+        <Tooltip anchorSelect=".postgres" place="bottom">
+            PostgreSQL
+        </Tooltip>
+        <a className="typescript"><SiTypescript className="text-3xl mx-1" /></a>
+        <Tooltip anchorSelect=".typescript" place="bottom">
+            TypeScript
+        </Tooltip>
+        <a className="node"><SiNodedotjs className="text-3xl mx-1" /></a>
+        <Tooltip anchorSelect=".node" place="bottom">
+            NodeJS
+        </Tooltip>
+        <a className="supabase"><SiSupabase className="text-3xl mx-1" /></a>
+        <Tooltip anchorSelect=".supabase" place="bottom">
+            Supabase
+        </Tooltip>
+        <a className="prisma"><SiPrisma className="text-3xl mx-1" /></a>
+        <Tooltip anchorSelect=".prisma" place="bottom">
+            Prisma
+        </Tooltip>
       </div>
     </div>
   );
