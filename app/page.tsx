135d9c6055b6f8ad4aa6285993dd9dba656ae2ee
Kyle Thornton <20633426+KyleMThornton@users.noreply.github.com>
2023-09-28 17:15:32 -0700

    Add about

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
+import About from "@/components/About"
 import Footer from "@/components/Footer"
 import HeaderText from "@/components/HeaderText"
 import NavBar from "@/components/NavBar"
 import Projects from "@/components/Projects"
+import Tech from "@/components/Tech"
 
 export default function Home() {
   return (
     <main>
       <NavBar />
       <HeaderText />
+      <About />
+      <Tech />
       <Projects />
       <Footer />
     </main>
---
+++ components/About.tsx
@@ -0,0 +1,10 @@
+export default function About() {
+    return (
+        <div className="flex justify-start container p-4">
+            <div className="flex flex-col md:w-4/5">
+                <h2>Tinkerer. Problem solver. Builder.</h2>
+                <h2>I make cool stuff and learn something new every day. Check out some of my work below!</h2>
+            </div>
+        </div>
+    )
+}
