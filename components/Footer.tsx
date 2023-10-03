df58fb741adb5b0d2852ab44fadcdc06cd6968d2
Kyle Thornton <20633426+KyleMThornton@users.noreply.github.com>
2023-10-03 11:55:17 -0700

    Add icons to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,13 +1,25 @@
+import {AiFillGithub, AiFillLinkedin} from 'react-icons/ai';
+import {MdEmail} from 'react-icons/md';
+import {HiDocumentText} from 'react-icons/hi';
+
 export default function Footer() {
+  const date = new Date();
+  const year = date.getFullYear();
+
   return (
     <footer className="footer footer-center p-4 bg-base-300 text-base-content">
-      <aside>
+      <aside className='flex'>
         <p>
           Made with ❤️ by{" "}
           <a href="https://github.com/KyleMThornton" target="_blank">
-            Kyle Thornton
+            Kyle Thornton 
           </a>
         </p>
+        <p> | © {year}</p>
+        <AiFillGithub className="text-xl hover:text-red-400" />
+        <AiFillLinkedin className="text-xl hover:text-red-400" />
+        <MdEmail className="text-xl hover:text-red-400" />
+        <HiDocumentText className="text-xl hover:text-red-400" />
       </aside>
     </footer>
   );
