0ca5127b7bb40a4c7f40442b68e5cb1d1a0edb78
Kyle Thornton <20633426+KyleMThornton@users.noreply.github.com>
2023-10-03 14:43:08 -0700

    Fix footer spacing

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,13 +9,8 @@ export default function Footer() {
   return (
     <footer className="footer footer-center p-4 bg-base-300 text-base-content">
       <aside className='flex'>
-        <p>
-          Made with ❤️ by{" "}
-          <a href="https://github.com/KyleMThornton" target="_blank">
-            Kyle Thornton 
-          </a>
-        </p>
-        <p> | © {year}</p>
+        <p>Made with ❤️ by Kyle Thornton</p>
+        <p> | {year}</p>
         <AiFillGithub className="text-xl hover:text-red-400" />
         <AiFillLinkedin className="text-xl hover:text-red-400" />
         <MdEmail className="text-xl hover:text-red-400" />
