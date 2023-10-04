a532a29b9b20db0164a4dca51da74778ac1c2685
Kyle Thornton <20633426+KyleMThornton@users.noreply.github.com>
2023-10-04 14:42:03 -0700

    Create contact comp

---
+++ components/Contact.tsx
@@ -0,0 +1,8 @@
+export default function Contact() {
+    return (
+        <div className="p-4 h-auto container">
+            <h2 className="text-4xl font-bold">Contact</h2>
+            <h3 className="pb-5">Get in touch with me!</h3>
+        </div>
+    )
+}
