e084ff1c216a4903b50fa887a6d546eff91ea957
Kyle Thornton <20633426+KyleMThornton@users.noreply.github.com>
2023-10-04 18:17:37 -0700

    Add color and hover effects to project cards

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -15,15 +15,15 @@ interface ProjectCardProps {
 export default function ProjectCard({ projectData }: ProjectCardProps) {
 
   return (
-    <div className="flex w-full">
+    <a href={projectData.link} target="_blank" rel="noopener noreferrer"><div className="flex w-full group">
       <div className="bg-white shadow-lg rounded-lg overflow-hidden">
         <img
-          className="w-full h-96 object-cover"
+          className="w-full h-96 object-cover group-hover:scale-105"
           src={projectData.image}
           alt={projectData.title}
         />
         <div className="p-4">
-          <a href={projectData.link} target="_blank" rel="noopener noreferrer"><h3 className="font-bold text-2xl mb-2">{projectData.title}</h3></a>
+          <a href={projectData.link} target="_blank" rel="noopener noreferrer"><h3 className="font-bold text-2xl mb-2 group-hover:text-transparent group-hover:bg-clip-text group-hover:bg-gradient-to-br from-pink-500 to-red-800">{projectData.title}</h3></a>
           <p className="text-gray-700 text-base">{projectData.description}</p>
           <div className="flex flex-wrap mt-3">
             {projectData.tech.map((tech) => (
@@ -37,6 +37,7 @@ export default function ProjectCard({ projectData }: ProjectCardProps) {
           </div>
         </div>
       </div>
-    </div>
+      </div>
+    </a>
   );
 }
