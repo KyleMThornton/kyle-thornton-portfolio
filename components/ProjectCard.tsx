9f4a944aa1c09e6b215423429fcbb2d5f7f762df
Kyle Thornton <20633426+KyleMThornton@users.noreply.github.com>
2023-10-06 11:59:37 -0700

    Fix Projects hydration issue

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -15,7 +15,7 @@ interface ProjectCardProps {
 export default function ProjectCard({ projectData }: ProjectCardProps) {
 
   return (
-    <a href={projectData.link} target="_blank" rel="noopener noreferrer"><div className="flex w-full group">
+    <div className="flex w-full group">
       <div className="bg-white shadow-lg rounded-lg overflow-hidden">
         <img
           className="w-full h-96 object-cover group-hover:scale-105 group-hover:drop-shadow-sm transition-all duration-200 ease-in-out"
@@ -23,7 +23,11 @@ export default function ProjectCard({ projectData }: ProjectCardProps) {
           alt={projectData.title}
         />
         <div className="p-4">
-          <a href={projectData.link} target="_blank" rel="noopener noreferrer"><h3 className="font-bold text-2xl mb-2 group-hover:text-transparent group-hover:bg-clip-text group-hover:bg-gradient-to-br from-pink-500 to-red-800">{projectData.title}</h3></a>
+          <a href={projectData.link} target="_blank" rel="noopener noreferrer">
+            <h3 className="font-bold text-2xl mb-2 group-hover:text-transparent group-hover:bg-clip-text group-hover:bg-gradient-to-br from-pink-500 to-red-800">
+              {projectData.title}
+            </h3>
+          </a>
           <p className="text-gray-700 text-base">{projectData.description}</p>
           <div className="flex flex-wrap mt-3">
             {projectData.tech.map((tech) => (
@@ -37,7 +41,6 @@ export default function ProjectCard({ projectData }: ProjectCardProps) {
           </div>
         </div>
       </div>
-      </div>
-    </a>
+    </div>
   );
 }
