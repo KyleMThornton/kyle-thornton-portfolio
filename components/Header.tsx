fa5f3573a9c3235ec1668de6a933bc5fb5c3a5c7
Kyle Thornton <20633426+KyleMThornton@users.noreply.github.com>
2023-10-06 15:44:14 -0700

    Header media fix

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ import Tech from "./Tech";
 
 export default function Header() {
     return (
-    <div className="min-h-screen md:min-h-0 pb-36">
+    <div className="min-h-screen sm:min-h-0 pb-36">
         <NavBar />
         <HeaderText />
         <About />
