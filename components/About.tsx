639055675efdd418f0d8b02551e52fe58d67d41b
Kyle Thornton <20633426+KyleMThornton@users.noreply.github.com>
2023-10-06 17:33:47 -0700

    Add about info

---
+++ components/About.tsx
@@ -0,0 +1,8 @@
+export default function About() {
+    return (
+        <div className="flex flex-col container p-4 pb-10 h-auto">
+            <h2 className="text-4xl font-semibold">About</h2>
+            <p> I'm Kyle Thornton, a passionate full-stack web developer with a strong foundation in front-end and back-end technologies. I recently completed a comprehensive full-stack web development certificate, and I'm excited to share my journey and expertise with you.</p>
+        </div>
+    )
+}
