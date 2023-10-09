2adf15b680975170c97612c9f0200e3d85b0a1e5
Kyle Thornton <20633426+KyleMThornton@users.noreply.github.com>
2023-10-09 12:29:26 -0700

    Add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Providers } from "./providers";
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
       <body className={`${inter.className} bg-zinc-100 dark:bg-zinc-950 text-zinc-950 dark:text-zinc-100`}>
         <Providers>
           {children}
+          <Analytics />
         </Providers>
       </body>
     </html>
