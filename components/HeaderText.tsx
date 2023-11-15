ec0c4866ab98d2d529b74760dd0e60d274bfd982
Kyle Thornton <kylemthornton@gmail.com>
2023-11-15 12:12:50 -0800

    Remove width limit

--- components/HeaderText.tsx
+++ components/HeaderText.tsx
@@ -25,14 +25,14 @@ export default function HeaderText() {
           👋🏻
         </h1>
         <h1 className="text-3xl md:text-4xl pb-3 font-semibold">
-          I&apos;m <span>Kyle Thornton</span>.
+          I&apos;m <span>Kyle Thornton</span>
         </h1>
         <h1 className="text-5xl md:text-6xl font-bold">
           A full stack web developer with a{" "}
           <span className="text-transparent bg-clip-text bg-gradient-to-br from-pink-500 to-red-800">
-            passion for technology
+            passion for creating
           </span>
-          .
+          
         </h1>
       </div>
     </header>
