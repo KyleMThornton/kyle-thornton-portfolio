c0fc734a0849c0dd4e9ff2d8088d61d73e745440
Kyle Thornton <kylemthornton@gmail.com>
2023-11-22 18:40:38 -0800

    Add contact section

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import Projects from "@/components/Projects";
@@ -7,6 +8,7 @@ export default function Home() {
     <div>
       <Header />
       <Projects />
+      <Contact />
       <Footer />
     </div>
   );
