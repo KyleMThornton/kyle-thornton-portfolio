9ee8e0fc436b66c239e5f91107cfaf37b2c68db2
Kyle Thornton <kylemthornton@gmail.com>
2023-12-24 14:09:55 -0800

    Add Resume to Navbar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -7,12 +7,15 @@ export default function NavBar() {
       <nav className="flex items-center justify-between">
         <a href=""><h1 className="text-3xl font-bold">&lt;KT /&gt;</h1></a>
         <div className="flex flex-row">
-          <ul className="flex items-center justify-between space-x-4 pr-2">
+          <ul className="flex items-center justify-between space-x-4 pr-2 text-lg">
             <li>
-              <Link href="#projects" scroll={true} className="text-lg hover:underline hover:font-semibold">Projects</Link>
+              <Link href='/files/KyleThorntonResume.pdf' className="hover:underline hover:font-semibold">Resume</Link>
             </li>
             <li>
-              <a href="mailto:kylemthornton@gmail.com" className="text-lg hover:underline hover:font-semibold">Contact</a>
+              <Link href="#projects" scroll={true} className="hover:underline hover:font-semibold">Projects</Link>
+            </li>
+            <li>
+              <a href="mailto:kylemthornton@gmail.com" className="hover:underline hover:font-semibold">Contact</a>
             </li>
           </ul>
           <ThemeSwitcher />
