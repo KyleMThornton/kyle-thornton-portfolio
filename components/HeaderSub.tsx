abedfcad0421d94b92e113bd18096d60fc7b10a8
Kyle Thornton <kylemthornton@gmail.com>
2023-12-25 22:53:30 -0800

    Reduce header sub text size

--- components/HeaderSub.tsx
+++ components/HeaderSub.tsx
@@ -2,8 +2,8 @@ export default function HeaderSub() {
   return (
     <div className="flex justify-start container p-4">
       <div className="flex flex-col ">
-        <h2 className="text-xl md:text-3xl font-semibold">Tinkerer. Problem solver. Builder.</h2>
-        <h2 className="text-xl md:text-3xl">
+        <h2 className="text-xl md:text-2xl font-semibold">Tinkerer. Problem solver. Builder.</h2>
+        <h2 className="text-xl md:text-2xl">
           I make cool stuff and learn something new every day. Check out some of
           my work below!
         </h2>
