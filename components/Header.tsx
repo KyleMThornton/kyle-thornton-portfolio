748fdc03c856a7a99e5ca9e4b6408d5a19f3f157
KyleMThornton <kylemthornton@gmail.com>
2024-01-17 21:33:21 -0800

    Add NewNavBar

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,17 +1,19 @@
 import HeaderSub from "./HeaderSub";
 import HeaderText from "./HeaderText";
 import NavBar from "./NavBar";
+import NewNavBar from "./NewNavBar";
 import Tech from "./Tech";
 
 export default function Header() {
-    return (
+  return (
     <div className="min-h-screen sm:min-h-0 pb-36">
-        <NavBar />
-        <HeaderText />
-        <div className="container flex-col">
-            <HeaderSub />
-            <Tech />
-        </div>
+      <NewNavBar />
+      {/* <NavBar /> */}
+      <HeaderText />
+      <div className="container flex-col">
+        <HeaderSub />
+        <Tech />
+      </div>
     </div>
-    )
+  );
 }
