49081de8fe5577b3d0b260344811c44f346d938f
KyleMThornton <kylemthornton@gmail.com>
2024-01-29 12:54:57 -0800

    Add Budget Dashboard data

--- components/ProjectData.tsx
+++ components/ProjectData.tsx
@@ -1,33 +1,52 @@
 const midpointProjectData = {
-    title: "midpoint",
-    description: "A webapp that takes two locations and returns a third location in the center between as well as a list of restaraunts and things to do there.",
-    tech: ["React", "NextJS", "Typescript", "Tailwind CSS"],
-    image: "/images/midpoint_thumb.jpg",
-    link: "https://midpoint.kylethornton.dev/"
-}
+  title: "midpoint",
+  description:
+    "A webapp that takes two locations and returns a third location in the center between as well as a list of restaraunts and things to do there.",
+  tech: ["React", "NextJS", "Typescript", "Tailwind CSS"],
+  image: "/images/midpoint_thumb.jpg",
+  link: "https://midpoint.kylethornton.dev/",
+};
 
 const justALittleRedditData = {
-    title: "Just a Little Reddit",
-    description: "A reddit client that allows you to set a limit to the number of posts you see so you don't scroll all day.",
-    tech: ["React", "Redux"],
-    image: "/images/reddit_thumb.jpg",
-    link: "https://just-a-little-reddit.kylethornton.dev/"
-}
+  title: "Just a Little Reddit",
+  description:
+    "A reddit client that allows you to set a limit to the number of posts you see so you don't scroll all day.",
+  tech: ["React", "Redux"],
+  image: "/images/reddit_thumb.jpg",
+  link: "https://just-a-little-reddit.kylethornton.dev/",
+};
 
 const entertainmentDatabaseData = {
-    title: "The Entertainment Database",
-    description: "A website that displays info about movies, tv shows, and actors. Data supplied from the TMDB api.",
-    tech: ["React", "NextJS", "Typescript", "Tailwind CSS"],
-    image: "/images/movies_thumb.jpg",
-    link: "https://movies.kylethornton.dev/"
-}
+  title: "The Entertainment Database",
+  description:
+    "A website that displays info about movies, tv shows, and actors. Data supplied from the TMDB api.",
+  tech: ["React", "NextJS", "Typescript", "Tailwind CSS"],
+  image: "/images/movies_thumb.jpg",
+  link: "https://movies.kylethornton.dev/",
+};
 
 const toDoData = {
-    title: "To Do List",
-    description: "A drag and drop to do list built using React and Typescript. Drag and drop functionality is provided by the dnd-kit library.",
-    tech: ["React", "Typescript"],
-    image: "/images/toDo_thumb.jpg",
-    link: "https://react-to-do-webapp.netlify.app/"
-}
+  title: "To Do List",
+  description:
+    "A drag and drop to do list built using React and Typescript. Drag and drop functionality is provided by the dnd-kit library.",
+  tech: ["React", "Typescript"],
+  image: "/images/toDo_thumb.jpg",
+  link: "https://react-to-do-webapp.netlify.app/",
+};
 
-export {midpointProjectData, justALittleRedditData, entertainmentDatabaseData, toDoData}
+const budgetData = {
+  title: "Budget Dashboard",
+  description:
+    "A budget dashboard to display someone's financial information all in one place. Data visualization is provided by the Tremor component library.",
+  tech: ["React", "Typescript", "Tailwind CSS"],
+  image: "/images/budgetDashboard_thumb.jpg",
+  link: "https://budget.kylethornton.dev/",
+};
+
+export {
+  midpointProjectData,
+  justALittleRedditData,
+  entertainmentDatabaseData,
+  toDoData,
+  budgetData,
+};
